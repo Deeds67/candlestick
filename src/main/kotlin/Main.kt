e27df10c6a5d5a7a7e44014a010c5c305a7a3d38
Pierre Marais <pierrem@live.co.za>
2022-06-27 17:56:01 +0200

    Fix serializing dates as timestamps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
@@ -29,3 +30,4 @@ val jackson: ObjectMapper =
   jacksonObjectMapper()
     .registerModule(JavaTimeModule())
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
