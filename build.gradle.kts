8ec8589ff385725d0604d29bca3015f8c67ef767
Pierre Marais <pierrem@live.co.za>
2022-06-27 19:31:53 +0200

    Added flyway migration plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
 plugins {
   kotlin("jvm") version "1.5.31"
   application
+  id("org.flywaydb.flyway") version "8.5.12"
+}
+
+fun getFromEnv(value: String): String? = emptyStringToNull(System.getenv(value))
+fun emptyStringToNull(value: String?): String? = if (value?.trim()?.isEmpty() != false) null else value
+
+flyway {
+  val dbURL = getFromEnv("DATABASE_HOST") ?: "localhost"
+  val dbUser = getFromEnv("DATABASE_USER") ?: "postgres"
+  val dbPassword = getFromEnv("DATABASE_PASSWORD") ?: "postgres"
+  val dbName = getFromEnv("DATABASE_NAME") ?: "postgres"
+  val dbPort = getFromEnv("DATABASE_PORT") ?: "5432"
+
+  url = "jdbc:postgresql://$dbURL:$dbPort/$dbName"
+  user = dbUser
+  password = dbPassword
 }
 
 application {
@@ -19,6 +35,7 @@ object DependencyVersions {
   const val http4k = "4.13.1.0"
   const val jackson = "2.13.+"
   const val mockk = "1.12.0"
+  const val postgres = "42.3.6"
 }
 
 dependencies {
@@ -37,6 +54,8 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${DependencyVersions.jackson}")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${DependencyVersions.jackson}")
   testImplementation("io.mockk:mockk:${DependencyVersions.mockk}")
+
+  implementation("org.postgresql:postgresql:${DependencyVersions.postgres}")
 }
 
 tasks.test {
