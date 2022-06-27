6a0196a37e28340531b5a3eac0c5d0c89915cc57
Pierre Marais <pierrem@live.co.za>
2022-06-27 19:47:29 +0200

    Added initial postgresql table definitions

---
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA IF NOT EXISTS data;
+
+CREATE TABLE IF NOT EXISTS data.instruments
+(
+    instrument_id SERIAL NOT NULL
+        CONSTRAINT instrument_pk
+            PRIMARY KEY,
+    isin           TEXT NOT NULL UNIQUE,
+    description    TEXT,
+    created_at     TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data.quotes
+(
+    isin TEXT NOT NULL
+        CONSTRAINT instrument_fk
+            REFERENCES data.instruments(isin) ON DELETE CASCADE,
+    price         NUMERIC(15,4) NOT NULL,
+    created_at    TIMESTAMPTZ NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS quotes_isin_created_at_idx
+    ON data.quotes (isin, created_at);
