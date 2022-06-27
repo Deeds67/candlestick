d5bd4f55514f5c5363a872992c20e6ebf49b53d4
Pierre Marais <pierrem@live.co.za>
2022-06-27 19:13:30 +0200

    Added 400 bad request route test

--- src/test/kotlin/RoutesTest.kt
+++ src/test/kotlin/RoutesTest.kt
@@ -42,4 +42,13 @@ class RoutesTest {
 
         assertThat(response, hasStatus(Status.OK).and(hasBody("""[{"open_timestamp":"2022-06-27T17:29:00Z","close_timestamp":"2022-06-27T17:30:00Z","open_price":243.5856,"high_price":243.9369,"low_price":235.1712,"closing_price":235.1712},{"open_timestamp":"2022-06-27T17:30:00Z","close_timestamp":"2022-06-27T17:31:00Z","open_price":243.5856,"high_price":243.9369,"low_price":222.222,"closing_price":222.1122}]""")))
     }
+
+    @Test
+    fun `ensure GET candlesticks returns bad request when no isin is specified`() {
+        val mockCandlestickManager = mockk<CandlestickManager>()
+        val routes = Routes(mockCandlestickManager)
+        val response = routes.getCandlesticks(Request(Method.GET, "/candlesticks"))
+
+        assertThat(response, hasStatus(Status.BAD_REQUEST).and(hasBody("{'reason': 'missing_isin'}")))
+    }
 }
